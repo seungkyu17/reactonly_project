b8d25e7fc59bf7575c94c7eafcad50a1894476f4
seungkyu17 <seungkyu17@naver.com>
2025-09-10 16:55:29 +0900

    bootstrap test

--- src/index.js
+++ src/index.js
@@ -7,14 +7,15 @@ import reportWebVitals from './reportWebVitals';
 //리액트 라우터를 사용하기 위해서 BrowserRouter를 App 외부에 감싸 주는 역할
 //App.js 파일을 index.js 파일이 포함하고 있으므로, BrouwserRouter 는 index.js 파일에 한번만 명시해주면 됩니다.
 import { BrowserRouter } from 'react-router-dom'; //추가
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 /* StrictMode 모드는 개발 도중 발생할 수 있는 무제를 꼼꼼히 감지하기 위하여 rendering을 두 번 실행해 줍니다.
 따라서, <React.StrictMode>를 삭제하도록 합니다. */
 root.render(
       <BrowserRouter>
-      <App/>
-      </BrowserRouter>      
+            <App />
+      </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/routes/menuData.js
+++ src/routes/menuData.js
@@ -48,6 +48,13 @@ export const menuData = [
             { path: '/use_effect_02', label: '이펙트 실습 02' },
             { path: '/use_effect_03', label: '이펙트 실습 03' },
         ]
-    }
+    },
+    {
+        chapter: "ch06. IT Cafe",
+        items: [
+            { path: '/coffee_bread_01', label: 'IT Cafe 01' },
+            { path: '/coffee_bread_02', label: 'IT Cafe 02' },
+        ]
+    },
     //여기에 4장, 5장... 내용을 계속 추가하면 됩니다.
 ];
